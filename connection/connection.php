07b44528ee92f000f11e405a370953b52815a4ad
Augusto Maza <debianmaza@gmail.com>
2023-10-03 12:40:17 -0300

    update #002 se cambia la conexion a la base de datos

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,18 @@
 <?php
-
+/*
 $db_host = 'slackzone.ddns.net';
 $db_user = 'root';
 $db_password = 'slack142';
 $db_basename = 'parinfo';
 $conn = mysqli_connect($db_host,$db_user,$db_password,$db_basename);
+*/
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'slack142';
+$db_basename = 'parinfo';
+$conn = mysqli_connect($db_host,$db_user,$db_password,$db_basename);
+
  
 
 
